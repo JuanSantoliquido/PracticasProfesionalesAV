ab7818a945299db4defd4690b13d4486fd097bd2
gianfiav <ggalli@americavirtualsa.com>
2024-05-15 17:00:43 -0300

    UsuarioDatos.cs y RegistroNeg.cs actualizados con los 5 SP

--- PracticaProfesional/WS.PracticasProfesionales.Datos/UsuarioDatos.cs
+++ PracticaProfesional/WS.PracticasProfesionales.Datos/UsuarioDatos.cs
@@ -10,7 +10,8 @@ namespace WS.PracticasProfesionales.Datos
     public class UsuarioDatos
     {
         #region  Usuarios
- 
+
+        //SP insert --> RegistarUsuario
         public int InsertarUsuario(DTOUsuarios usuario)
         {
 
@@ -38,6 +39,118 @@ namespace WS.PracticasProfesionales.Datos
 
 
 
+
+        //SP insert --> RegistarLogin
+        public int InsertarLogin(DTOLogins login)
+        {
+
+            System.Data.SqlClient.SqlConnection oCnn = Conexion.ObtenerConnexionSQL();
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("registrarLogin", oCnn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@usuarioId", login.UsuarioId));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@direccionIP", login.DireccionIP));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@exitoso", login.Exitoso));
+                
+            int result = -1;
+            cmd.Parameters.Add("@Result", System.Data.SqlDbType.Int).Direction = System.Data.ParameterDirection.Output;
+
+            oCnn.Open();
+            cmd.ExecuteNonQuery();
+            result = Convert.ToInt32(cmd.Parameters["@Result"].Value);
+            oCnn.Close();
+
+            return result;
+        }
+
+
+
+
+
+        //SP SELECT ->  obtenerUsuarios --- Esta bien?
+        public string ObtenerUsuarios()
+        {
+            string resultado = string.Empty;
+
+            System.Data.SqlClient.SqlConnection oCnn = Conexion.ObtenerConnexionSQL();
+            oCnn.Open();
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("obtenerUsuarios", oCnn);
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                using (System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        resultado = reader["ID"].ToString();
+                    }
+                }
+            };
+
+            return resultado;
+        }
+
+
+
+
+        //SP SELECT -> obtenerUsuarioPorNombreUsuario --- Esta bien?
+        public string ObtenerUsuarioPorNombreUsuario()
+        {
+            string resultado = string.Empty;
+
+            System.Data.SqlClient.SqlConnection oCnn = Conexion.ObtenerConnexionSQL();
+            oCnn.Open();
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("obtenerUsuarioPorNombreUsuario", oCnn);
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                using (System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        resultado = reader["ID"].ToString();
+                    }
+                }
+            };
+
+            return resultado;
+        }
+
+
+
+        //SP UPDATE -> actualizarDatosPersonales --- Esta bien?
+        public int actualizarDatosPersonales(DTODatosPersonales datosPersonales)
+        {
+
+            System.Data.SqlClient.SqlConnection oCnn = Conexion.ObtenerConnexionSQL();
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("actualizarDatosPersonales", oCnn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@usuarioId", datosPersonales.UsuarioId));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@nombre", datosPersonales.Nombre));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@apellido", datosPersonales.Apellido));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@fechaNacimiento", datosPersonales.FechaNacimiento));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@generoId", datosPersonales.GeneroId));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@direccion", datosPersonales.Direccion));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@nacionalidad", datosPersonales.Nacionalidad));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@pais", datosPersonales.Pais));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@dni", datosPersonales.DNI));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@mail", datosPersonales.Mail));
+
+
+            int result = -1;
+            cmd.Parameters.Add("@Result", System.Data.SqlDbType.Int).Direction = System.Data.ParameterDirection.Output;
+
+            oCnn.Open();
+            cmd.ExecuteNonQuery();
+            result = Convert.ToInt32(cmd.Parameters["@Result"].Value);
+            oCnn.Close();
+
+            return result;
+        }
 
 
 
--- PracticaProfesional/WS.PracticasProfesionales.Negocio/RegistroNeg.cs
+++ PracticaProfesional/WS.PracticasProfesionales.Negocio/RegistroNeg.cs
@@ -9,7 +9,6 @@ using WS.PracticasProfesionales.Datos.Models;
 
 namespace WS.PracticasProfesionales.Negocio
 {
-
     public class RegistroNeg
     {
 
@@ -17,7 +16,7 @@ namespace WS.PracticasProfesionales.Negocio
 
         UsuarioDatos UsuarioDB = new UsuarioDatos();
 
-
+        //Negocio SP RegistarLogin - InsertarLogin
         public int InsertarUsuario(DTOUsuarios usuario)
         {
             int rta = -1;
@@ -31,7 +30,72 @@ namespace WS.PracticasProfesionales.Negocio
             }
             return rta;
         }
-      
+
+        //Negocio SP RegistrarLogin - InsertarLogin
+        public int InsertarLogin(DTOLogins login)
+        {
+            int rta = -1;
+            try
+            {
+                rta = UsuarioDB.InsertarLogin(login);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            return rta;
+        }
+
+
+        //SP SELECT ->  obtenerUsuarios --- Esta bien?
+        public string ObtenerUsuarios()
+        {
+            string rta = "";
+            try
+            {
+                rta = UsuarioDB.ObtenerUsuarios();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rta;
+        }
+
+        //SP SELECT -> obtenerUsuarioPorNombreUsuario --- Esta bien?
+
+        public string obtenerUsuarioPorNombreUsuario()
+        {
+            string rta = "";
+            try
+            {
+                rta = UsuarioDB.ObtenerUsuarioPorNombreUsuario();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rta;
+        }
+
+        //SP UPDATE -> DatosPersonales --- Esta bien?
+
+        public int ActualizarApplicationPoolDB(DTODatosPersonales datosPersonales)
+        {
+            int rta = -1;
+            try
+            {
+                rta = UsuarioDB.actualizarDatosPersonales(datosPersonales);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            return rta;
+        }
+
+
+
         #endregion
 
     }
