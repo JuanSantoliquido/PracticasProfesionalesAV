88a7db04924d35f91efb0746c775b161c9f8fbbd
gianfiav <ggalli@americavirtualsa.com>
2024-05-15 15:53:29 -0300

    Arreglo del usuariodatos y negocio

---
+++ PracticaProfesional/WS.PracticasProfesionales.Datos/UsuarioDatos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WS.PracticasProfesionales.Datos.Models;
+
+namespace WS.PracticasProfesionales.Datos
+{
+    public class UsuarioDatos
+    {
+        #region  Usuarios
+ 
+        public int InsertarUsuario(DTOUsuarios usuario)
+        {
+
+            System.Data.SqlClient.SqlConnection oCnn = Conexion.ObtenerConnexionSQL();
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("registrarUsuario", oCnn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@NombreUsuario", usuario.NombreUsuario));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Email", usuario.email));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Contraseña", usuario.Contraseña));
+            cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@PerfilId", usuario.PerfilId));
+
+            int result = -1;
+            cmd.Parameters.Add("@Result", System.Data.SqlDbType.Int).Direction = System.Data.ParameterDirection.Output;
+
+            oCnn.Open();
+            cmd.ExecuteNonQuery();
+            result = Convert.ToInt32(cmd.Parameters["@Result"].Value);
+            oCnn.Close();
+
+            return result;
+        }
+
+
+
+
+
+
+
+        #endregion
+
+    }
+}
---
+++ PracticaProfesional/WS.PracticasProfesionales.Negocio/RegistroNeg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WS.PracticasProfesionales.Datos;
+using WS.PracticasProfesionales.Datos.Models;
+
+
+namespace WS.PracticasProfesionales.Negocio
+{
+
+    public class RegistroNeg
+    {
+
+        #region Usuarios
+
+        UsuarioDatos UsuarioDB = new UsuarioDatos();
+
+
+        public int InsertarUsuario(DTOUsuarios usuario)
+        {
+            int rta = -1;
+            try
+            {
+                rta = UsuarioDB.InsertarUsuario(usuario);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            return rta;
+        }
+      
+        #endregion
+
+    }
+}
