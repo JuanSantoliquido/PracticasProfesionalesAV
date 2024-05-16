019ef7af7fa1812628a7dc549f7654a9114c33ad
gianfiav <ggalli@americavirtualsa.com>
2024-05-16 15:17:59 -0300

    capa de presentacion, registro terminado

---
+++ PracticaProfesional/WS.PracticasProfesionales/GestionarUsuarios.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GestionarUsuarios.asmx.cs" Class="WS.PracticasProfesionales.WebService1" %>
---
+++ PracticaProfesional/WS.PracticasProfesionales/GestionarUsuarios.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WS.PracticasProfesionales.Datos.Models;
+using WS.PracticasProfesionales.Negocio;
+
+namespace WS.PracticasProfesionales
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class GestionarUsuarios : WebService
+    {
+        //Libreria para gestionar registros, añadí el archivo log4net.config a la presentacion
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(GestionarUsuarios).Name);
+
+        #region WEBSITE
+
+
+        [WebMethod(Description = "InsertarUsuario: Registra un usuario.")]
+        public int InsertarUsuario(DTOUsuarios usuario)
+        {
+
+            int respuesta = -1;
+
+            RegistroNeg usuarios = new RegistroNeg();
+            try
+            {
+                if (usuario.PerfilId == "") { usuario.PerfilId = null; }
+                respuesta = usuarios.InsertarUsuario(usuario);
+            }
+            catch (System.Data.SqlClient.SqlException exsql)
+            {
+
+                //log.Fatal(exsql.Message + "***" + exsql.StackTrace);
+            }
+
+            return respuesta;
+        }
+    }
+    #endregion
+
+}
